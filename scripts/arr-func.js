64c5465c2589c0818f58d93dfaccd51021f2e35c
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-29 15:01:59 +0300

    array functions

---
+++ scripts/arr-func.js
@@ -0,0 +1,12 @@
+function sum(a, b){
+    return a + b
+}
+
+let summ = (a, b) => a + b;
+console.log(summ(3,5))
+
+let isPositive = number => number >= 0
+console.log(isPositive(9))
+
+let randomNumber = () => Math.random
+console.log(randomNumber(1, 12))
