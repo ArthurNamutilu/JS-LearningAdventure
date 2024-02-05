a89e020a313fb361dbf4100da628408e118be3b1
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-05 22:58:31 +0300

    -mObjects as object properties

---
+++ objects/obj-objProperty.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const person = {
+    name: {  //  Objects as object properties
+        first: "Arthur",
+        last: "Namutilu",
+    },
+    age: 24,
+    countries: ['Kenya', 'Germany', 'UK', 'USA'],
+};
+
+console.log(person.name.last);
