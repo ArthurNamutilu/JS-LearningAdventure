e79078ececd7e37caf0d52a6004a6ee012a2e28c
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-14 11:04:51 +0300

    -mrest parameter

---
+++ es6/rest-param.js
@@ -0,0 +1,9 @@
+// the rest parameter allows you to represent an indefinite number of arguments as an array
+const sum = (...args) => {  // ... js rest parameter
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+      total += args[i];
+    }
+    return total;
+  }
+  console.log(sum(1,2,3,4))
